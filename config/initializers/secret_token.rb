dd7e2d7d61dcb910d49733fe9289c5006afb75ef
pritish.mhapankar <pritish.mhapankar@icicletech.com>
2012-12-02 16:19:46 +0530

    Ruby Application Created Successfully

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitstudy::Application.config.secret_token = '5d073005dc0c246e39077fa11b200c2ea9dbf6c6c3483b6756a0600540a685402ee9df0c3f085a2cbf0c18cc90c841b81762e97fa0ca8ae721bf25b6627c920f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gitstudy::Application.config.session_store :cookie_store, key: '_gitstudy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gitstudy::Application.config.session_store :active_record_store
